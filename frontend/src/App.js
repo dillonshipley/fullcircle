3a71d701a0ca4c8253fab60223085ea9624ef05b
dshipley <dillonshipley@proton.me>
2023-10-30 23:12:56 -0700

    added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Full Circle Nutrition
+    </div>
+  );
+}
+
+export default App;
