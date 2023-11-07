64667c0e8e281c1c8b8db4a839fb7a4e0c19ab6e
dillonshipley <dillonshipley@proton.me>
2023-11-06 21:43:35 -0800

    added form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+
+function MyForm() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       Full Circle Nutrition
+      <MyForm />
     </div>
+
+    
   );
 }
 
