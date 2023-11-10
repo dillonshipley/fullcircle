14f59b1a9b0700d90172982f53f645a46d8bfb54
dillonshipley <dillonshipley@proton.me>
2023-11-10 13:23:47 -0800

    adding a two handled slider - part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import InitialForm from './initialForm/InitialForm';
 
-function MyForm() {
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
 
 function App() {
   return (
     <div className="App">
       Full Circle Nutrition
-      <MyForm />
+      <InitialForm />
     </div>
 
     
--- frontend/src/initialForm/InitialForm.js
+++ frontend/src/initialForm/InitialForm.js
@@ -6,8 +6,13 @@ import Goal             from './Full/Goal'
 import ActivityLevel    from './Full/ActivityLevel'
 import MacroSettings    from './Full/MacroSettings'
 
+import Welcome          from './Shared/Welcome'
+import Restrictions     from './Shared/Restrictions'
+import Schedule         from './Shared/Schedule'
+import Finalize         from './Shared/Finalize'
 
-export default function Form() {
+
+export default function InitialForm() {
     const [step, setStep] = useState(0);
     const [totalSteps, setTotalSteps] = useState(0);
     const [user_mode, setMode] = useState("");
@@ -31,14 +36,15 @@ export default function Form() {
         //Schedule / cheat meals
         <div>
             {!welcomeComplete && <Welcome complete = {setWelcomeComplete} setMode = {setMode} />}
-            {(user_mode == "custom" && step == 0) && <ManualSetup change = {setStep}/>}
-            {(user_mode == "full" && step == 0) && <Biography change = {setStep}/>}
-            {(user_mode == "full" && step == 1) && <ActivityLevel change = {setStep}/>}
-            {(user_mode == "full" && step == 2) && <Goal change = {setStep}/>}
-            {(user_mode == "full" && step == 3) && <MacroSettings change = {setStep}/>}
-            {(step == 10) && <Restrictions />}
-            {(step == 11) && <Schedule />}
-            {(step = 12) && <Finalize />}
+            <ManualSetup />
+            {(user_mode === "custom" && step === 0) && <ManualSetup change = {setStep}/>}
+            {(user_mode === "full" && step === 0) && <Biography change = {setStep}/>}
+            {(user_mode === "full" && step === 1) && <ActivityLevel change = {setStep}/>}
+            {(user_mode === "full" && step === 2) && <Goal change = {setStep}/>}
+            {(user_mode === "full" && step === 3) && <MacroSettings change = {setStep}/>}
+            {(step === 10) && <Restrictions />}
+            {(step === 11) && <Schedule />}
+            {(step === 12) && <Finalize />}
         </div>
 
     )
