2cd2be1c920405e45ab62d52087281aea336a88d
dshipley <dillonshipley@proton.me>
2023-11-26 20:06:08 -0800

    get ingredients call written

--- frontend/src/admin/addForms/MealLogger.js
+++ frontend/src/admin/addForms/MealLogger.js
@@ -1,5 +1,19 @@
-export default function MealLogger(){
-    return (
-        <h4>Add A Meal</h4>
-    );
+export default class MealLogger {
+
+
+    getIngredients = async () => {
+        const response = await fetch(process.env.REACT_APP_API_URL + "get/ingredientList", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log(response.json)
+    }
+
+    render(){
+        return (
+            <h4>Add A Meal</h4>
+        );
+    }
 }
