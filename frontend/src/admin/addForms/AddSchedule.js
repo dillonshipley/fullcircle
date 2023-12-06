914ce51a35fecc123b1459b6fd83dd015c2262f1
dillonshipley <dillonshipley@proton.me>
2023-12-05 20:51:18 -0800

    added preliminary config for days in schedule

--- frontend/src/admin/addForms/AddSchedule.js
+++ frontend/src/admin/addForms/AddSchedule.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import {Form} from 'react-bootstrap';
 
-function Day({index, minimized, meals}){
-    return (
-        <div>
-            <h3>{"Day " + (index + 1)}</h3>
-            <Form.Group>
-                <Form.Label>Meals</Form.Label>
-                <Form.Control 
-                    type = "text"
-                    placeholder = "Between 1 and 6"
-                    autoComplete = "off" />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Snacks</Form.Label>
-                <Form.Control 
-                    type = "text"
-                    placeholder = "Between 0 and 5"
-                    autoComplete = "off" />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Breakfast?</Form.Label>
-                <Form.Control 
-                    type = "checkbox"
-                    placeholder = "Between 0 and 5"
-                    autoComplete = "off" />
-            </Form.Group>
+function Day({index, minimized, meals, show}){
 
-        </div>
+    if(!minimized){
+        return (
+            <div>
+                <h5>{"Day " + (index + 1)}</h5>
+                <p onClick = {() => {console.log("click");show(index)}}> + </p>
+            </div> 
+        );
+    } else {
+        return (
+            <div>
+                <h5>{"Day " + (index + 1)}</h5>
+                <Form.Group>
+                    <Form.Label>Meals</Form.Label>
+                    <Form.Control 
+                        type = "text"
+                        placeholder = "Between 1 and 6"
+                        autoComplete = "off" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Snacks</Form.Label>
+                    <Form.Control 
+                        type = "text"
+                        placeholder = "Between 0 and 5"
+                        autoComplete = "off" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Breakfast?</Form.Label>
+                    <Form.Control 
+                        type = "checkbox"
+                        placeholder = "Between 0 and 5"
+                        autoComplete = "off" />
+                </Form.Group>
 
-    );
+            </div>
+        );
+    }
+
 }
 
 const Counter = React.memo(({ count, handleIncrement, handleDecrement }) => {
@@ -48,8 +58,10 @@ export default class AddSchedule extends React.Component{
         super(props);
         this.state = {
             days: 7,
-            dayArray: [undefined, undefined, undefined, undefined, undefined, undefined, undefined]
+            dayArray: [undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+            shown: null
         }
+        this.daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     }
 
     increaseDays = () => {
@@ -73,13 +85,37 @@ export default class AddSchedule extends React.Component{
         // Your form submission logic here
       };
 
+      setShownDay = (e) => {
+        this.setState({shown: e})
+      }
+
+      handleSelectChange = (e) => {
+        this.setState({firstDay: e});
+      }
+
     render(){
         return (
             <Form onSubmit ={this.handleSubmit}>
+                <p>What day do you want to start on?</p>
+                    <select id = "dropdown" onChange={() => this.handleSelectChange()}>
+                        {
+                            this.daysOfWeek.map((day) => (
+                                <option key = {day} value = {day} >{day}</option>
+                            ))
+                        }
+                    </select>
+
+                <p>How many days a week do you want to plan for?</p>
                 <Counter count = {this.state.days} handleDecrement={() => this.decreaseDays()} handleIncrement={() => this.increaseDays()}/>
                 {
                     this.state.dayArray.map((key, index) => (
-                        <Day key = {index} index = {index} minimized = {this.state.shown === index} meals = {1}/>
+                        <Day 
+                            key = {index} 
+                            index = {index} 
+                            minimized = {this.state.shown === index} 
+                            meals = {1}
+                            show = {() => this.setShownDay(index)}
+                        />
                     ))
                 }
             </Form>   
