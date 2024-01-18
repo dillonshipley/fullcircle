a42756cd135b51e3d966d7e24f24fa9f89936175
dshipley <dillonshipley@proton.me>
2024-01-17 21:26:27 -0800

    added nutrition label and going to fill in the rest

--- frontend/src/admin/AddIngredient.js
+++ frontend/src/admin/AddIngredient.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
-import {Container, Form} from 'react-bootstrap';
+import {Container, Row, Form, Col} from 'react-bootstrap';
+import NutritionLabel from '../tools/NutritionLabel';
 
 
 export default function AddIngredient(){
     const [term, setSearchTerm] = useState(null);
     const [ingredients, setIngredients] = useState(null);
+    const [selectedIngredientID, setSelectedIngredientID] = useState(null);
+    const [selectedIngredientName, setSelectedIngredientName] = useState(null);
+    const [selectedNutrients, setSelectedNutrients] = useState(null);
     
     const search = async (event) => {
         event.preventDefault();
@@ -18,28 +22,61 @@ export default function AddIngredient(){
         });
 
         const x = await response.json();
-        const array = x.map(x => x.description);
-        console.log(array);
+        console.log(x);
+        const array = x.map(x => [x.description, x.id]);
         setIngredients(array);
+    }
+
+    const handleRadioChange = async (name, FDCID) => {
+        const response = await fetch(process.env.REACT_APP_API_URL + "tf/specificIngredientFDC", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"id": FDCID})
+        });
+
+        const nutrients = await response.json();
+        console.log(nutrients);
+        setSelectedNutrients(nutrients);
+        setSelectedIngredientID(FDCID);
+        setSelectedIngredientName(name);
+    }
+
+    const NutrientsDisplay = () => {
+        <div>{selectedNutrients[0]}</div>
     }
 
     return (
         <>
             <Container>
-                <Form onSubmit ={(e) => search(e)}>
-                    <Form.Group>
-                        <Form.Control 
-                                type = "text"
-                                placeholder = "Find an ingredient"
-                                onChange = {(e) => setSearchTerm(e.target.value)}
-                                autoComplete = "off" 
-                        />
-                    </Form.Group>
-                </Form>
+                <Row>
+                <Col>
+                    <Form onSubmit ={(e) => search(e)}>
+                        <Form.Group>
+                            <Form.Control 
+                                    type = "text"
+                                    placeholder = "Find an ingredient"
+                                    onChange = {(e) => setSearchTerm(e.target.value)}
+                                    autoComplete = "off" 
+                            />
+                        </Form.Group>
+                        {ingredients != null && ingredients.slice(0, 10).map((x, index) => (
+                        <div key = {index} onClick = {() => handleRadioChange(x[0], x[1])}>{x[0]}</div>
+                        ))}
+                    </Form>
+                </Col>
+                <Col>
+                    {selectedNutrients != null && <h2>{selectedIngredientName}</h2>}
+                    {selectedNutrients != null && <NutritionLabel nutrients={selectedNutrients}/>}
+                </Col>
+
+
+                </Row>
+
+
             </Container>
-            {ingredients != null && ingredients.map((x, index) => (
-                <p key = {index}>{x}</p>
-            ))}
+
         </>
     );
 }
